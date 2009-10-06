6664680a22bd3ceaa24d9da86fc012e13d2440fa
beepscore <support@beepscore.com>
2009-10-06 00:07:12 -0700

    Minor revision, clarify comments.

--- dissector.m
+++ dissector.m
@@ -2,23 +2,27 @@
 // 
 // UW CP-120 Certificate in iPhone and Cocoa Development
 // Q1 Intro to Programming in Objective-C and the Cocoa Framework.
-// Homework assignment 28 Sep 09 part 1.
+// Homework assignment 1a.
 
 #import <Foundation/Foundation.h>
 
+// the method declaration might be better placed in the header file,
+// but I don't know how to deal with a precompiled header yet.
 void dissectByRecursion(NSString *aString);
+
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // Beginner style unit tests!  I don't know how to write them yet.
+    // Beginner style unit tests!  I don't know how to write tests yet.
     // NSString *dissectableString = @"";  // passed
     // NSString *dissectableString = @"a"; // passed
     // NSString *dissectableString = @"ab"; // passed
     // NSString *dissectableString = @"1234\5\"6";  // passed, \5 returns blank line
+    
     NSString *dissectableString = @"this is a longer string for some fun";
     
-    NSLog(@"This string will be dissected: %@", dissectableString);
+    NSLog(@"Dissecting: %@", dissectableString);
     // use singular and plural.  Alternatively could use  ?@"":@"s"
     NSLog(@"The string contains %d %@. \n\n",
           [dissectableString length],
@@ -40,6 +44,7 @@ int main (int argc, const char * argv[]) {
 
 // This method appears slightly more complicated than looping, may use more memory
 void dissectByRecursion(NSString *aString) {
+    
     int aStringLength = [aString length];
 
     if (0 == aStringLength) {
